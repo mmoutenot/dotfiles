23a6069068d844efbca65dd61ad2e4a17889a75a
mmoutenot <mmoutenot@gmail.com>
2013-10-20 21:32:16 -0400

    remove .config and update readme

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,17 @@ function doIt() {
   rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
     --exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
   source ~/.bash_profile
+
   sh .brew
+
   sh .osx
+
+  # install bundle and vim bundles
   git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
   vim -E -c BundleInstall -c q
+
+  # install oh-my-zsh
+  curl -L https://github.com/bpinto/oh-my-fish/raw/master/tools/install.sh | sh
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
   doIt
